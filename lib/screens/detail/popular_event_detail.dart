b7355f3a2a1f1a230d516eacb42e726173c4358e
Trần Gia Hoàng <105185593+HoangTranGia@users.noreply.github.com>
2022-06-15 00:33:06 +0700

    Create popular_event_detail.dart

---
+++ lib/screens/detail/popular_event_detail.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PopularEventDetail extends StatelessWidget {
+  const PopularEventDetail({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
