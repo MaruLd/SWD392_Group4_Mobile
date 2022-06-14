a93ee27cb20034a39b4386a82dcec0a245248119
Trần Gia Hoàng <105185593+HoangTranGia@users.noreply.github.com>
2022-06-15 00:33:21 +0700

    Update body.dart

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,20 @@
+import 'package:evsmart/screens/home/components/event_page_body.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget{
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          //header
+          Container(),
+          //body
+          Expanded(child: SingleChildScrollView(
+            child: EventPageBody(),
+          ),
+          ),
+        ],
+      )
+    );
   }
 }
