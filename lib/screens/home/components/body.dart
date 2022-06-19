512462a0b425c97686552fd422ee9b60deab6f67
52 Blue <tttai0620@gmail.com>
2022-06-19 23:39:40 +0700

    BLOC Architecture and API calls setup

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -15,43 +15,44 @@ class Body extends StatelessWidget {
             child: EventPageBody(),
           ),
         ),
-        // Container(
-        //   margin: EdgeInsets.only(top: 45, left: 15),
-        //   padding: EdgeInsets.only(),
-        //   child: Row(
-        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //     children: [
-        //       Column(
-        //         children: [
-        //           Text(
-        //             "Evsmart",
-        //             style: TextStyle(
-        //                 fontWeight: FontWeight.bold,
-        //                 fontSize: 35,
-        //                 color: kPrimaryColor),
-        //           ),
-        //         ],
-        //       ),
-        //       Container(
-        //         width: 45,
-        //         height: 45,
-        //         child: Icon(
-        //           Icons.search,
-        //           color: Colors.white,
-        //         ),
-        //         margin: EdgeInsets.only(right: 15),
-        //         decoration: BoxDecoration(
-        //           borderRadius: BorderRadius.circular(30),
-        //           color: kPrimaryColor,
-        //         ),
-        //       )
-        //     ],
-        //   ),
-        // ),
-        // //body
-        // Container(
-        //   child: EventPageBody(),
-        // )
+        Container(
+          margin: EdgeInsets.only(top: 45, left: 15),
+          padding: EdgeInsets.only(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Evsmart",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      // color: kPrimaryColor
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                width: 45,
+                height: 45,
+                child: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                margin: EdgeInsets.only(right: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  // color: kPrimaryColor,
+                ),
+              )
+            ],
+          ),
+        ),
+        //body
+        Container(
+          child: EventPageBody(),
+        )
       ],
     ));
   }
