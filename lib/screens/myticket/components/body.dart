f97606f35e063e0a731c02d297ec80fdeeb35487
Trần Gia Hoàng <105185593+HoangTranGia@users.noreply.github.com>
2022-06-26 21:22:21 +0700

    TabBar Myticket

--- lib/screens/myticket/components/body.dart
+++ lib/screens/myticket/components/body.dart
@@ -1,8 +1,18 @@
+import 'package:evsmart/screens/constraint.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: TabBar(
+        labelColor: kPrimaryColor,
+        indicatorColor: kPrimaryColor,
+        tabs: [
+        Tab(text: 'Event',),
+        Tab(text: 'Voucher',),
+      ],),
+    );
   }
 }
