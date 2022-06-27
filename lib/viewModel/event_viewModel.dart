545c76274c11faf11c54b252e2dd4aa56c5ee829
Maru <accnumberthree@gmail.com>
2022-06-27 07:38:48 +0700

    update

---
+++ lib/viewModel/event_viewModel.dart
@@ -0,0 +1,24 @@
+import 'package:evsmart/models/DAO/event_DAO.dart';
+import 'package:evsmart/models/DTO/event_model.dart';
+import 'package:evsmart/screens/enums.dart';
+import 'package:evsmart/viewModel/baseModel.dart';
+
+class EventViewModel extends BaseModel {
+  EventDAO? eventDAO;
+  List<Event>? listEvent;
+  EventViewModel() {
+    eventDAO = EventDAO();
+  }
+
+  Future<void> getEvent() async {
+    try {
+      setState(ViewStatus.Loading);
+      listEvent = await eventDAO?.getAllEvent();
+      await Future.delayed(Duration(microseconds: 500));
+      setState(ViewStatus.Completed);
+    } catch (e) {
+      listEvent = null;
+      setState(ViewStatus.Completed);
+    }
+  }
+}
