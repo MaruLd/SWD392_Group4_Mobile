c3299b90ab3c1fed9673acdbc2c77e3b8b79eb1a
Trần Gia Hoàng <105185593+HoangTranGia@users.noreply.github.com>
2022-07-11 07:18:34 +0700

    Update ticket_viewModel.dart

--- lib/viewModel/ticket_viewModel.dart
+++ lib/viewModel/ticket_viewModel.dart
@@ -1,6 +1,4 @@
-import 'package:evsmart/models/DAO/event_DAO.dart';
 import 'package:evsmart/models/DAO/ticket_DAO.dart';
-import 'package:evsmart/models/DTO/event_model.dart';
 import 'package:evsmart/models/DTO/ticket_model.dart';
 import 'package:evsmart/screens/enums.dart';
 import 'package:evsmart/viewModel/base_viewModel.dart';
