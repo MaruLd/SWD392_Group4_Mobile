bf5c7a313a06d8deee1a7cbf0fa2544c65c431e6
Trần Gia Hoàng <105185593+HoangTranGia@users.noreply.github.com>
2022-07-11 07:18:21 +0700

    Update Search

--- lib/screens/search/SearchData.dart
+++ lib/screens/search/SearchData.dart
@@ -2,58 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:line_icons/line_icons.dart';
 
-class Search extends StatelessWidget{
+class Search extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container();
   }
 }
-class DataSearch extends SearchDelegate<String>{
 
-final events = [
-  "Mây Lang Thang",
-  "Như một lời chia tay",
-  "LA VELA SAIGON",
-  "Sài Gòn Tếu",
-];
+class DataSearch extends SearchDelegate<String> {
+  final events = [
+    "Mây Lang Thang",
+    "Như một lời chia tay",
+    "LA VELA SAIGON",
+    "Sài Gòn Tếu",
+  ];
 
-final recentEvents = [
-  "Mây Lang Thang",
-  "Như một lời chia tay",
-];
+  final recentEvents = [
+    "Mây Lang Thang",
+    "Như một lời chia tay",
+  ];
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     // actions for app bar
-   return [IconButton(icon: Icon(Icons.clear), onPressed: (){
-     query ="";
-   })];
-
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     // leading icon on the left of the app bar
-    return IconButton(icon: AnimatedIcon(
-      icon: AnimatedIcons.menu_arrow,
-      progress: transitionAnimation,)
-      , onPressed: (){Navigator.pop(context);});
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        });
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // show some result based on the selection
+    // Tra ve trang detail cua event sau khi nhan vao`
     throw UnimplementedError();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final suggestionList = query.isEmpty?recentEvents:events;
- return ListView.builder(itemBuilder: (context, index)=> ListTile(
-      leading: LineIcon(LineIcons.moneyCheck),
-   title: Text(suggestionList[index]),
- ), itemCount: suggestionList.length,
- );
+    final suggestionList = query.isEmpty
+        ? recentEvents
+        : events.where((p) => p.startsWith(query)).toList();
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: (){
+          showResults(context);
+        },
+        leading: LineIcon(LineIcons.moneyCheck),
+        title: RichText(
+          text: TextSpan(
+              text: suggestionList[index].substring(0, query.length),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+         children: [
+           TextSpan(
+           text: suggestionList[index].substring(query.length),
+           style: TextStyle(color: Colors.grey))
+         ])
+          ),
+        ),
+      itemCount: suggestionList.length,
+    );
   }
-
 }
